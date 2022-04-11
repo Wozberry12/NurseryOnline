797d9b6aaf40016c92c6ad813a683245d5fb9b10
benTKam <56923669+benTKam@users.noreply.github.com>
2022-04-10 21:54:26 -0400

    Train Commit 6

--- public/sellerScript.js
+++ public/sellerScript.js
@@ -6,4 +6,13 @@ class fruit{
         this.quantity = quantity;
     }
 }
-
+//this is a base thing for whenever we figure out the webAPI stuff 
+//this list below is assuming we have a list of fruit objects from an access database we made
+var databaseFruits = []; 
+function fillFruitForSale(){
+    var forSaleDiv = document.getElementById("forSaleContents");
+    forSaleDiv.innerHTML("");
+    for(var i = 0; i<databaseFruits.length; i++){
+        //the way I did this for a previous lab was horrible so if you guys have a better way lets do that :)
+    }
+}
