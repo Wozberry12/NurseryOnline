4d51b218899d6b89d568e1768f77e4e7ee884d2b
benTKam <56923669+benTKam@users.noreply.github.com>
2022-04-10 21:32:37 -0400

    Train Commit 3

---
+++ public/sellerScript.js
@@ -0,0 +1,9 @@
+class fruit{
+    constructor(name, price, image, quantity){
+        this.name = name;
+        this.price = price;
+        this.image = image;
+        this.quantity = quantity;
+    }
+}
+
