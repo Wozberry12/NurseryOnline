14d49cadb5c344bd73298ef9efcb8279f7587fb0
benTKam <56923669+benTKam@users.noreply.github.com>
2022-04-11 10:13:00 -0400

    Update fruit.cs

--- public/fruit.cs
+++ public/fruit.cs
@@ -1,13 +1,26 @@
-public class Fruit{
-    string name;
-    double price;
-    int quantity;
-    string img;
-    public Fruit(name, price, quantity, img){
-        this.name = name;
-        this.price = price;
-        this.quantity = quantity;
-        this.img = img;
-    }
+using System;
 
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Web;
+
+ 
+
+using Newtonsoft.Json.Serialization;
+namespace Core2WebAPI{
+    public class Fruit{
+        string name;
+        double price;
+        int quantity;
+        string img;
+        public Fruit(name, price, quantity, img){
+            this.name = name;
+            this.price = price;
+            this.quantity = quantity;
+            this.img = img;
+        }
+
+    }
 }
