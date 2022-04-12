785218c429c402d08a7b063bcdb0e7f7abfbaef2
MarcoKosasih <103138161+MarcoKosasih@users.noreply.github.com>
2022-04-12 14:30:26 -0400

    Fruit Display

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,8 @@
 <script>
 	import NavigationBar from './components/NavigationBar.svelte'
-	
-
+	import FeedbackList from './components/FeedbackList.svelte'
+	import FruitPage from './components/FruitPage.svelte'
+	import {writable} from "svelte/store";
 	
 	let feedback = [
 		{
@@ -23,17 +24,34 @@
 			description: 'this is a tulip',
 		}
 	]
+	class Fruit{
+		constructor(name, fruitType, price, quanity, img){
+			this.name =name;
+			this.fruitType = fruitType;
+			this.price = price;
+			this.quanity = quanity;
+			this.img = img;
+		}
+	}
+	
+	let goldenDelicous = new Fruit("Golden Delicous Apple", "Apple", 4.50, 25, "imgString");
+	let grannySmith = new Fruit("Granny Smith Apple", "Apple", 3.75, 30, "imgString");
+	let cantelope = new Fruit("Cantelope", "Cantelope", 6.99, 15, "imgString");
+	let pineApple = new Fruit("Pineapple", "Pineapple", 5.45, 29, "imgString");
+	var fruitList = new Fruit(goldenDelicous, grannySmith, cantelope, pineApple);
+
+	const stored = localStorage.content
 
 	
-	
-
+	export const content = writable<Fruit>(JSON.parse(localStorage.getItem('fruit')))
+	content.subscribe((value) => localStorage.fruit = JSON.stringify(value))
 </script>
 
 <main>
 	<h1>Test</h1>
 	<div id="navDiv"><NavigationBar /></div>
 	<!--<FruitPage /> -->
-	
+
 	<footer>
 		<p>this is a footer</p>
 	</footer>
