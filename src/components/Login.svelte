44de60db4cefe8ab92d12098979605246b3cf7a8
benTKam <56923669+benTKam@users.noreply.github.com>
2022-04-12 15:00:12 -0400

    Added profiles Class

--- src/components/Login.svelte
+++ src/components/Login.svelte
@@ -1,4 +1,15 @@
-
+<script>
+    class profile{
+        constructor(username, password){
+            this.username;
+            this.password;
+        }
+    }
+    var account1 = new profile("marco", "marcopassword");
+    var account2 = new profile("ben", "benpassword");
+    var localAccounts = [account1, account2];
+    localStorage.setItem("Accounts", JSON.stringify(localAccounts));
+</script>
 <div id="titleBar">
     <h1>Login to your Nursey Online Account Below</h1>
 </div>
@@ -8,7 +19,6 @@
         <input type="text" id="username" name="username">
         <label for="password">Password</label>
         <input type="text" id="password" name="password"><br>
-        <input type="submit" id="submit" value="Submit" onclick="loginConfirmation_Click();">
+        <input type="submit" id="submit" value="Submit">
     </form>
 </div>
-
