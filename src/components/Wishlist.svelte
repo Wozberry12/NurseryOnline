cc0897384e7c662e8455acf64f7ae67cfb3534bd
benTKam <56923669+benTKam@users.noreply.github.com>
2022-04-14 16:09:11 -0400

    wishlist

--- src/components/Wishlist.svelte
+++ src/components/Wishlist.svelte
@@ -1,7 +1,11 @@
 <script>
+    var profile = JSON.parse(sessionStorage.getItem("currentlyLogedIn"));
+    var itemList = profile.wishList;
 
 </script>
 
 <div class="userWishList">
-    
+    {#each itemList as item}
+        <h1>{item.name}</h1>
+    {/each}
 </div>
