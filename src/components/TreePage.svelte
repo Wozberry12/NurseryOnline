4da25b9a45fc5916cd32fcf3a88c6f44428f4977
MarcoKosasih <103138161+MarcoKosasih@users.noreply.github.com>
2022-04-15 12:26:38 -0400

    add item to tree

--- src/components/TreePage.svelte
+++ src/components/TreePage.svelte
@@ -1,11 +1,11 @@
 <script>
     class Item{
-		constructor(id, name, Type, price, quanity, img){
+		constructor(id, name, Type, price, quantity, img){
 			this.id = id;
 			this.name =name;
 			this.Type = Type;
 			this.price = price;
-			this.quanity = quanity;
+			this.quantity = quantity;
 			this.img = img;
 		}
 	}
@@ -14,7 +14,8 @@
 	var dragon = new Item(2002, "Dragon Tree", "Caesalpinia", 3.75, 30, "https://i5.walmartimages.com/asr/0149db71-df23-4dc8-9995-205fb5acfbd2.284e3ef8e9998c0dfc4330141cf5f0ee.jpeg");
 	var palm = new Item(2003, "Parlor Palm", "Chamaedora elegans", 6.99, 15, "https://images.thdstatic.com/productImages/9fcb04c0-aff8-406c-afbe-ce886879ce58/svn/indoor-plants-4-palm-parlor-64_1000.jpg");
 	var candelabra = new Item(2004, "African Candelabra", "Pineapple", 5.45, 29, "https://i.etsystatic.com/20034067/r/il/ff969a/1852472466/il_fullxfull.1852472466_m4eb.jpg");
-	var treeList = [bonsai, dragon, palm, candelabra];
+	var olive = new Item(2005, "European Olive", "Olive", 99.99, 19, "");
+	var treeList = [bonsai, dragon, palm, candelabra, olive];
     localStorage.setItem("treeList", JSON.stringify(treeList));
 
     const handleWish = (tree) => {
@@ -41,11 +42,11 @@
         <div class="container">
 			<img  class="itemImage" src={tree.img} alt={tree.name}>
 			<div class="itemDescription">
-				Name:{tree.name}
+				Name: {tree.name}
 				<br>
 				Price: {tree.price}
 				<br>
-				Quantity {tree.quantity}
+				Quantity: {tree.quantity}
 				<br>
 			</div>
 			<div class="itemButton">
