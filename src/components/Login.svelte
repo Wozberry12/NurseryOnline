31d0ce373d9e94a86ed2c5c694a7b2deaa3f5457
benTKam <56923669+benTKam@users.noreply.github.com>
2022-04-18 13:31:16 -0400

    delete Account

--- src/components/Login.svelte
+++ src/components/Login.svelte
@@ -61,6 +61,15 @@
         localStorage.setItem("Accounts", JSON.stringify(storageAccounts));
 
     }
+    const handleDeleteAccount = (usernameLogin, passwordLogin) => {
+        let storageAccounts = JSON.parse(localStorage.getItem("Accounts"));
+        for(let i = 0; i < storageAccounts.length; i++){
+            if(storageAccounts[i].username == usernameLogin && storageAccounts[i].password == passwordLogin){
+                storageAccounts.splice(i, 1);
+            }
+        }
+        localStorage.setItem("Accounts", JSON.stringify(storageAccounts));
+    }
     
 </script>
 <div class="titleImage">
@@ -81,6 +90,7 @@
     <div class="loginButton">
         <button class="addWishList" on:click={() => handleCreateAccount(usernameLogin, passwordLogin)}>Create Account</button> 
         <button class="addWishlist" on:click={() => handleLogin(usernameLogin, passwordLogin)}>Login</button>
+        <button class="addWishlist" on:click={() => handleDeleteAccount(usernameLogin, passwordLogin)}>Delete Account</button>
     </div>
 </div>
 <style>
