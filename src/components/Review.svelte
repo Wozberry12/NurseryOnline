6e6268711c29890181f7b60aac97abb9631862be
MarcoKosasih <marco105.k@gmail.com>
2022-04-17 21:03:10 -0400

    revise reviews and styles

--- src/components/Review.svelte
+++ src/components/Review.svelte
@@ -9,9 +9,11 @@
         <p class="reviewRating">
             {reviewItem.rating}
         </p>
+        <p class="reviewName">{reviewItem.nameOfReviewer}</p>
+        <br>
+        <br>
         <div class="reviewContainer">
-            {reviewItem.nameOfReviewer}
-            {reviewItem.description}
+            <p>{reviewItem.description}</p>
         </div>
         {/each}
     </div>  
@@ -19,19 +21,33 @@
 
 <style>
     .reviewInfo{
-        width: 22%;
+        width: 20%;
         float:right;
-        border: dashed 2px red;
+        border: solid 5px #333;
+        border-radius: 5px;
+        background-color: rgb(0, 138, 0);
+        font-family: Verdana, Geneva, Tahoma, sans-serif;
+        font-weight: bold;
     }
 
     .reviewRating{
+        float: right;
         border: groove;
-        background-color: white;
+        background-color: lightsalmon;
         width:10%;
+        margin-right: 20px;
+        border-radius: 50%;
         height: 10%;
     }
     .reviewContainer{
-        border: groove;
-        margin: 10px;
+        padding:1%;
+        float: left;
+        font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+        text-align: left;
+    }
+    .reviewName{
+        float: left;
+        padding: 10px;
+        width: auto;
     }
 </style>
